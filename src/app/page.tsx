5c9179662e0126862e977dcc2b3c82af16023c5b
kailanesarah <kailanesarahpro@gmail.com>
2025-08-12 13:32:00 -0300

    refactor: refactor page.tsx
    refatorada a estrutura da página principal, agora ela redireciona direto para o login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { requireSession } from "@/modules/auth/auth-utils";
-import LoginButton from "./signin/components/UI/button-login";
+import { redirect } from "next/navigation";
+import { ROUTES } from "@/constants/urls";
 
 const WelcomePage = async () => {
+
   const session = await requireSession();
+
+  if (!session) {
+    redirect(ROUTES.PUBLIC.SIGNIN); 
+  }
+  redirect(ROUTES.PRIVATE.DASHBOARD);
 
   return (
     <main className="h-screen flex flex-col justify-center items-center bg-blue-50 px-4">
       <h1 className="text-5xl font-bold text-gray-800 mb-4">
-        Bem-vindo(a), faça seu login para continuar!
-        <LoginButton/>
+        Bem-vindo(a), você já está logado!
       </h1>
     </main>
   );
