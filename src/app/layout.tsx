ef497e4d60ea5874e32493b4ed19b4629cfc5f8e
alexsmagalhaes <106816507+alexsmagalhaes@users.noreply.github.com>
2025-08-17 11:35:24 -0300

    feat: add toast component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import "@/styles/globals.css";
 import "@/styles/material-icons/index.css";
 import localFont from "next/font/local";
 import { usePathname } from "next/navigation";
+import { Toaster } from "react-hot-toast";
 
 const fontAspektaVariable = localFont({
   src: "../../public/fonts/aspekta-variable.woff2",
@@ -24,6 +25,29 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className={`${fontAspektaVariable.variable} antialiased`}>
         {mostrarSidebar && <SidebarComponent />}
+        <aside>
+          {" "}
+          <Toaster
+            gutter={16}
+            position="bottom-right"
+            toastOptions={{
+              style: {
+                transition: "0ms",
+                height: "auto",
+                maxWidth: "none",
+                padding: 0,
+                margin: 0,
+                boxShadow: "none",
+              },
+            }}
+            containerStyle={{
+              margin: 0,
+              padding: 0,
+              bottom: "3rem",
+              right: "3rem",
+            }}
+          />
+        </aside>
         <div className={mostrarSidebar ? "sm:ml-64" : ""}>
           <div className={mostrarSidebar ? "mt-16" : ""}>{children}</div>
         </div>
--- src/app/signin/components/UI/signin-form.tsx
+++ src/app/signin/components/UI/signin-form.tsx
@@ -9,22 +9,24 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Icon } from "@/components/ui/icon";
 import { Input } from "@/components/ui/input";
 import { InputPassword } from "@/components/ui/input-password";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import z from "zod";
 
-export default function SignInForm() {
-  const formSchema = z.object({
-    email: z.email("Insira um email válido"),
-    password: z.string("Senha é obrigatório"),
-  });
+const formSchema = z.object({
+  email: z.string().email("Insira um email válido"),
+  password: z.string().min(1, "Senha é obrigatória"),
+});
 
+export default function SignInForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
+      password: "",
     },
   });
 
@@ -34,34 +36,38 @@ export default function SignInForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="Email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Senha</FormLabel>
-              <FormControl>
-                <InputPassword {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <div className="space-y-3">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Senha</FormLabel>
+                <FormControl>
+                  <InputPassword {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type="submit">
+          Entrar no sistema <Icon name="login" />
+        </Button>
       </form>
     </Form>
   );
