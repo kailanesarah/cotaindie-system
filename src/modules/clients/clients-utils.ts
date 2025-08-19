97917646446e76f009ec34318a47f0515388967e
kailanesarah <kailanesarahpro@gmail.com>
2025-08-19 12:32:54 -0300

    refactor(modules/clients): refatoramento do módulo de clients
    Foi removido o arquivo respository, tal como em auth, agora o repository utilizado é o presente em
    sheets. Além disso, também foram padronizadas as respostas de sucesso e erro com a lib boom

--- src/modules/clients/clients-utils.ts
+++ src/modules/clients/clients-utils.ts
@@ -2,18 +2,18 @@ import type { ClientInput } from "./clients-schema";
 
 export function mapSheetDataToClients(data: any[][]): ClientInput[] {
   return data.map((row) => ({
-    id: row[0] ?? "",
-    name: row[1] ?? "",
-    category: row[2] ?? "",
-    cpf: row[3] ?? "",
-    cnpj: row[4] ?? "",
-    phone: row[5] ?? "",
-    email: row[6] ?? "",
-    city: row[7] ?? "",
-    zipCode: row[8] ?? "",
-    neighborhood: row[9] ?? "",
-    address: row[10] ?? "",
-    complement: row[11] ?? "",
-    notes: row[12] ?? "",
+    client_id: row[0] ?? "",
+    client_name: row[1] ?? "",
+    client_category: row[2] ?? "",
+    client_cpf: row[3] ?? "",
+    client_cnpj: row[4] ?? "",
+    client_phone: row[5] ?? "",
+    client_email: row[6] ?? "",
+    client_city: row[7] ?? "",
+    client_zipCode: row[8] ?? "",
+    client_neighborhood: row[9] ?? "",
+    client_address: row[10] ?? "",
+    client_complement: row[11] ?? "",
+    client_notes: row[12] ?? "",
   }));
 }
