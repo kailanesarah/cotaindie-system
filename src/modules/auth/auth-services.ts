d46aacc60afa12f776b6c36309ae41b500b90f49
kailanesarah <kailanesarahpro@gmail.com>
2025-08-19 12:30:42 -0300

    refactor(modules/auth): refatoração do modulo auth
    Removido o arquivo repository, agora se utiliza o arquivo centralizado no módulo sheets, além disso,
    foi feita a padronização das respostas de sucesso e erro com a lib boom.

--- src/modules/auth/auth-services.ts
+++ src/modules/auth/auth-services.ts
@@ -1,44 +1,65 @@
-import { usersFromSheetRepository } from "@/modules/auth/auth-repository";
-import { type User, userSchema } from "@/types/user";
+import { errorsResponse } from "@/utils/errors-messages";
+import { successResponse } from "@/utils/success-messages";
+import { getSheetDataService } from "../google-sheets/sheets-service";
+import { type User, userSchema } from "./auth-user";
 
-export async function dataGETService() {
+const BASE_SHEET = {
+  sheetId: process.env.SHEET_ID!,
+  sheetRange: "bd-users!A2:D",
+  sheetName: "bd-users",
+};
+
+export async function getUserService() {
   try {
-    const data = await usersFromSheetRepository();
-    return data;
-  } catch (error) {
-    console.error("Erro no serviceDataGET:", error);
-    throw error;
+    const data = await getSheetDataService(BASE_SHEET);
+    const users: User[] = data
+      .map((row) =>
+        userSchema.safeParse({
+          user_id: row[0],
+          user_name: row[1],
+          user_email: row[2],
+          user_password: row[3],
+        })
+      )
+      .filter(
+        (result): result is { success: true; data: User } => result.success
+      )
+      .map((result) => result.data);
+
+    return successResponse(users, 200);
+  } catch (error: any) {
+    throw errorsResponse(500, "Erro ao buscar usuários", error);
   }
 }
 
-export async function loginService(
-  email: string,
-  password: string,
-): Promise<User | null> {
+export async function loginService(email: string, password: string) {
   try {
-    const data_user: any[][] = await usersFromSheetRepository();
+    const data_user: any[][] = await getSheetDataService(BASE_SHEET);
 
     const users: User[] = data_user
       .map((row) =>
         userSchema.safeParse({
-          id: row[0],
-          username: row[1],
-          email: row[2],
-          password: row[3],
-        }),
+          user_id: row[0],
+          user_name: row[1],
+          user_email: row[2],
+          user_password: row[3],
+        })
       )
       .filter(
-        (result): result is { success: true; data: User } => result.success,
+        (result): result is { success: true; data: User } => result.success
       )
       .map((result) => result.data);
 
     const user = users.find(
-      (user) => user.email === email && user.password.trim() === password,
+      (u) => u.user_email === email && u.user_password.trim() === password
     );
 
-    return user || null;
-  } catch (error) {
-    console.error("Erro ao validar usuário:", error);
-    return null;
+    if (!user) {
+      throw errorsResponse(401, "Credenciais inválidas");
+    }
+
+    return successResponse(user, 200);
+  } catch (error: any) {
+    throw errorsResponse(500, "Erro interno ao validar login", error);
   }
 }
