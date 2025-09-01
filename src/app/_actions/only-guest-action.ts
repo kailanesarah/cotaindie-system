53ec99b0c647470d04108b31cdd0f31cd7ed3105
kailanesarah <kailanesarahpro@gmail.com>
2025-09-01 15:14:21 -0300

    refactor(_actions/only-guest-actions.ts | only-users-action.ts): refatora actions
    Refatora actions gerais de autenticação, agora a validação será feita com supabase

--- src/app/_actions/only-guest-action.ts
+++ src/app/_actions/only-guest-action.ts
@@ -1,13 +1,13 @@
 "use server";
 
 import { ROUTES } from "@/constants/urls";
-import { auth } from "@/modules/auth/api/auth";
+import { requireSession } from "@/modules/supabase/supabase-auth-service";
 import { redirect } from "next/navigation";
 
-export const onlyGuestAction = async () => {
-  const session = await auth();
+export const onlyGuestAction = async (): Promise<void> => {
+  const session = await requireSession();
 
-  if (session?.user) {
+  if (session) {
     redirect(ROUTES.PRIVATE.DASHBOARD);
   }
 };
