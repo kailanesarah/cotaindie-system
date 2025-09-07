03e14cd6e2f29a7f8ee6a7367ae98bb860a3a876
alexsmagalhaes <106816507+alexsmagalhaes@users.noreply.github.com>
2025-09-06 23:12:15 -0300

    feat: add metric cards

--- src/app/(private)/(navgation)/dashboard/page.tsx
+++ src/app/(private)/(navgation)/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { PageContent } from "../_components/page-content";
 import {
   PageHeader,
   PageHeaderContent,
@@ -6,10 +7,22 @@ import {
   PageHeaderIcon,
   PageHeaderTitle,
 } from "../_components/page-header";
+import { PageMain } from "../_components/page-main";
+import {
+  MetricCard,
+  MetricFooter,
+  MetricIcon,
+  MetricLabel,
+  MetricPieChart,
+  MetricPoint,
+  MetricTitle,
+  MetricWrap,
+  MetricWrapper,
+} from "./_components/chart";
 
 export default async function DashboardPage() {
   return (
-    <main>
+    <PageMain>
       <PageHeader>
         <PageHeaderContent>
           <PageHeaderIcon name="dashboard_customize" />
@@ -21,6 +34,43 @@ export default async function DashboardPage() {
           </PageHeaderHeading>
         </PageHeaderContent>
       </PageHeader>
-    </main>
+      <PageContent className="grid grid-cols-3 items-start gap-4">
+        <MetricCard>
+          <MetricWrapper>
+            <MetricWrap>
+              <MetricLabel>Pedidos finalizados</MetricLabel>
+              <MetricTitle>
+                42{" "}
+                <span className="text-title-light ml-[0.375rem] text-sm">
+                  Vendas
+                </span>
+              </MetricTitle>
+              <MetricFooter className="flex gap-3">
+                <MetricPoint className="bg-black-default" text="Total - 67" />
+                <MetricPoint className="bg-b-light" text="Orçados - 33" />
+              </MetricFooter>
+            </MetricWrap>
+            <MetricPieChart values={{ sold: 40, quoted: 21 }} />
+          </MetricWrapper>
+          <MetricIcon name="order_approve" />
+        </MetricCard>
+        <MetricCard>
+          <MetricWrap>
+            <MetricLabel>Receita de vendas</MetricLabel>
+            <MetricTitle>R$ 86.842,43</MetricTitle>
+            <MetricFooter>Pedidos fechados - últimos 30 dias</MetricFooter>
+          </MetricWrap>
+          <MetricIcon name="bar_chart_4_bars" />
+        </MetricCard>
+        <MetricCard>
+          <MetricWrap>
+            <MetricLabel>Lucro estimado</MetricLabel>
+            <MetricTitle>R$ 23.842,43</MetricTitle>
+            <MetricFooter>Lucro estimado - últimos 30 dias</MetricFooter>
+          </MetricWrap>
+          <MetricIcon name="trending_up" />
+        </MetricCard>
+      </PageContent>
+    </PageMain>
   );
 }
--- src/types/order.d.ts
+++ src/types/order.d.ts
@@ -1,7 +1,9 @@
+type Status = "approved" | "open";
+
 interface Order {
   id: string;
   code: string;
   name: string;
   client: Pick<Client, "code" | "name">;
-  status: "approved" | "open";
+  status: Status;
 }
