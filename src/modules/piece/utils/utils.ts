bc637e612ce14add5bbcbb0dfae36649765d13af
kailanesarah <kailanesarahpro@gmail.com>
2025-09-11 21:17:41 -0300

    feat(modules/piece): adiciona utilitário de piece
    Adiciona utilitário em piece que filtra todas as peças de um projeto

---
+++ src/modules/piece/utils/utils.ts
@@ -0,0 +1,34 @@
+// ----------------------------
+// Busca peças de um projeto (genérico)
+// ----------------------------
+
+import type { Piece } from "@/modules/cuttingPlan/utils/packing-utils";
+import { getPiecesByProjectId } from "../piece-services";
+
+export async function fetchPieces(project_id: string): Promise<Piece[]> {
+  try {
+    const response = await getPiecesByProjectId(project_id);
+
+    const pieces: Piece[] =
+      response?.data?.map((item: any) => ({
+        w: item.piece_width,
+        h: item.piece_height,
+        name: item.piece_name ?? "Sem nome",
+        category_id: item.product_id?.product_category ?? "Sem categoria", // <-- obrigatório
+      })) ?? [];
+
+    if (pieces.length === 0) {
+      throw new Error("Nenhuma peça encontrada para o projeto");
+    }
+
+    console.log(
+      "Categorias das peças capturadas:",
+      pieces.map((p) => p.category_id),
+    );
+
+    return pieces;
+  } catch (error) {
+    console.error("Erro ao buscar peças do projeto:", error);
+    throw new Error("Falha ao buscar peças do projeto");
+  }
+}
