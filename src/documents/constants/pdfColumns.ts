93f8b0ff00f54bae0c4584a5e86778bd2160b680
Kailane Sarah <75998609+kailanesarah@users.noreply.github.com>
2025-09-24 09:35:45 -0300

    feat(documents/constants): adiciona constantes referente as colunas das tabelas dos pdfs

---
+++ src/documents/constants/pdfColumns.ts
@@ -0,0 +1,33 @@
+export const COLUNAS_CLIENTES = [
+  "Nome",
+  "Código",
+  "CPF/CNPJ",
+  "Telefone",
+  "Cidade",
+  "E-mail",
+  "Endereço",
+];
+export const COLUNAS_MATERIAIS = [
+  "Código",
+  "Material",
+  "Qtde int",
+  "Qtd",
+  "Medida",
+  "Valor unitário",
+  "Total",
+];
+export const COLUNAS_PROJETOS = [
+  "Código",
+  "Nome do Projeto",
+  "Quantidade",
+  "Status",
+];
+export const COLUNAS_CUSTOS = [
+  "Código",
+  "Equipe",
+  "Quantidade",
+  "Valor",
+  "Total",
+];
+
+export const COLUNAS_PECAS = ["Código", "Nome", "Quantidade"];
