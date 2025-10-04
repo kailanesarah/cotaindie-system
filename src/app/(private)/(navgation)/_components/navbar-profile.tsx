c24422bae4a4bcb38c0d3d06d4fd0fb4b23db333
alexsmagalhaes <106816507+alexsmagalhaes@users.noreply.github.com>
2025-10-04 18:50:06 -0300

    feat: add menu mobile

--- src/app/(private)/(navgation)/_components/navbar-profile.tsx
+++ src/app/(private)/(navgation)/_components/navbar-profile.tsx
@@ -5,37 +5,29 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Icon } from "@/components/ui/icon";
+import { getInitialLetters } from "../_utils/get-initial-letters";
 import { DropdownLogout } from "./dropdown-button";
 
 interface INavbarProfile {
-  readonly name: string;
-  readonly role: "administrador" | "funcionário";
-  readonly imageUrl?: string;
+  profile: { name: string; role: string; imageUrl?: string };
 }
 
-export const NavbarProfile = ({
-  name,
-  imageUrl,
-  role,
-}: Readonly<INavbarProfile>) => {
-  const initials = name
-    .split(" ")
-    .map((word) => word[0])
-    .join("")
-    .slice(0, 2)
-    .toUpperCase();
+export const NavbarProfile = ({ profile }: INavbarProfile) => {
+  const initials = getInitialLetters(profile.name);
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <button className="group flex cursor-pointer items-center gap-3 focus:outline-none">
           <Avatar>
-            <AvatarImage src={imageUrl} alt={name} />
+            <AvatarImage src={profile.imageUrl} alt={profile.name} />
             <AvatarFallback>{initials}</AvatarFallback>
           </Avatar>
           <div className="-mt-0.5 flex flex-col gap-0 text-left">
-            <span className="text-title-light font-semibold">{name}</span>
-            <span className="text-body-light text-xs">{role}</span>
+            <span className="text-title-light font-semibold">
+              {profile.name}
+            </span>
+            <span className="text-body-light text-xs">{profile.role}</span>
           </div>
           <Icon
             name="keyboard_arrow_down"
--- src/app/(private)/(navgation)/layout.tsx
+++ src/app/(private)/(navgation)/layout.tsx
@@ -1,21 +1,33 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { auth } from "@/modules/auth/api/auth";
 import type { ReactNode } from "react";
 import { AppSidebar } from "./_components/app-sidebar";
 import { Navbar } from "./_components/navbar";
+import { NavbarMobile } from "./_components/navbar-mobile";
 import { sidebarLinks } from "./_constants/menu-links";
 
-export default function NavigationLayout({
+export default async function NavigationLayout({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
+  const session = await auth();
+  const user = session?.user ?? { name: "", imageUrl: "", url: "" };
+
+  const profile = {
+    name: user.name ?? "",
+    role: "Administrador",
+    imageUrl: "",
+  };
+
   return (
     <SidebarProvider>
       <div className="hidden lg:block">
         <AppSidebar menuItems={sidebarLinks} />
       </div>
-      <div className="flex grow flex-col">
-        <Navbar />
+      <div className="relative flex grow flex-col">
+        <NavbarMobile profile={profile} />
+        <Navbar profile={profile} />
         {children}
       </div>
     </SidebarProvider>
