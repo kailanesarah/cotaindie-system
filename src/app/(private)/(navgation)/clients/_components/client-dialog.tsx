54999f9be815a2841f792ddd95e4470a18fc6a78
alexsmagalhaes <106816507+alexsmagalhaes@users.noreply.github.com>
2025-10-05 17:16:05 -0300

    feat: add client dialog responsiveness

--- src/app/(private)/(navgation)/clients/_components/client-dialog.tsx
+++ src/app/(private)/(navgation)/clients/_components/client-dialog.tsx
@@ -16,12 +16,19 @@ export const ClientDialog = ({ client }: { client?: Client }) => {
   return (
     <DialogContent size="large">
       <DialogHeader>
-        <DialogIcon name="article_person" />
+        <div className="flex items-center gap-3">
+          <DialogIcon name="article_person" className="hidden lg:block" />
+          {client?.id && (
+            <Badge className="h-5 text-[0.6875rem] lg:hidden">{id}</Badge>
+          )}
+        </div>
         <DialogHeaderContent>
           <DialogTitle>{id ? name : "Adicione um novo cliente"}</DialogTitle>
           {client?.id && (
             <DialogDescription className="flex gap-3">
-              <Badge className="h-5 text-[0.6875rem]">{id}</Badge>
+              <Badge className="hidden h-5 text-[0.6875rem] lg:block">
+                {id}
+              </Badge>
               Alterações só serão refletidas em cotações novas.
             </DialogDescription>
           )}
