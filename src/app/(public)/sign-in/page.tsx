a3389822cd705c17c99525e288946abbb22b6af3
alexsmagalhaes <106816507+alexsmagalhaes@users.noreply.github.com>
2025-10-20 07:36:42 -0300

    fix: update docs page users permissions

--- src/app/(public)/sign-in/page.tsx
+++ src/app/(public)/sign-in/page.tsx
@@ -1,7 +1,10 @@
+import { onlyGuestAction } from "@/app/_actions/only-guest-action";
 import { SignInBox } from "./_components/sign-in-box";
 import { SupportBox } from "./_components/support-box";
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  await onlyGuestAction();
+
   return (
     <main className="flex grow items-center justify-center">
       <div className="my-6 flex w-full flex-col gap-4 lg:max-w-[30rem]">
