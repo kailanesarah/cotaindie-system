d14dc521ea4f5f8a2bffbc185f302020637faefa
alexsmagalhaes <106816507+alexsmagalhaes@users.noreply.github.com>
2025-11-01 14:22:37 -0300

    feat: add dinamic toast position for desktop and mobile

--- src/app/_components/toast.tsx
+++ src/app/_components/toast.tsx
@@ -1,14 +1,39 @@
 "use client";
 
 import { useMediaQuery } from "@/hooks/use-media-query";
+import { usePathname } from "next/navigation";
 import { Toaster } from "react-hot-toast";
 
 export const Toast = () => {
   const isSmallScreen = useMediaQuery("(max-width: 1023px)");
+  const pathname = usePathname();
+
+  const margins: Record<string, { desktop: string; mobile: string }> = {
+    "/order": { desktop: "6.5rem", mobile: "4.5rem" },
+    "/materials": { desktop: "5rem", mobile: "4.5rem" },
+    "/orders": { desktop: "5rem", mobile: "4.5rem" },
+    "/clients": { desktop: "5rem", mobile: "4.5rem" },
+  };
+
+  const matchedRoute = Object.keys(margins).find((route) =>
+    pathname.startsWith(route),
+  );
+
+  let marginBottom: string;
+
+  if (matchedRoute) {
+    const marginConfig = margins[matchedRoute];
+
+    marginBottom = isSmallScreen ? marginConfig.mobile : marginConfig.desktop;
+  } else {
+    marginBottom = isSmallScreen ? "1.5rem" : "0.5rem";
+  }
+
+  const toasterKey = `${pathname}-${isSmallScreen ? "mobile" : "desktop"}`;
 
   return (
     <Toaster
-      key={isSmallScreen ? "mobile" : "desktop"}
+      key={toasterKey}
       gutter={16}
       position={isSmallScreen ? "bottom-center" : "bottom-right"}
       toastOptions={{
@@ -26,6 +51,7 @@ export const Toast = () => {
       containerStyle={{
         margin: 0,
         padding: 0,
+        marginBottom,
       }}
     />
   );
