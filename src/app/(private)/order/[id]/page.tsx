cbfb18c4a0b5b01ddafa7af6bee1e4a5788e4be1
alexsmagalhaes <106816507+alexsmagalhaes@users.noreply.github.com>
2025-11-09 15:45:44 -0300

    feat: integrate order to order page bar

--- src/app/(private)/order/[id]/page.tsx
+++ src/app/(private)/order/[id]/page.tsx
@@ -12,6 +12,7 @@ import {
   FormTitle,
 } from "../_components/form-block";
 import { OrderIncludedForm } from "../_components/order-included-form";
+import { OrderMenu } from "../_components/order-menu";
 import { OrderPaymentForm } from "../_components/order-payment-form";
 import {
   OrderProjects,
@@ -32,72 +33,78 @@ export default async function OrderPage({ params }: Readonly<IOrderPage>) {
 
   let orderId: string | null = null;
 
+  let code = undefined;
+
   if (id && id !== "new") {
     const supabase = await supabaseServer();
 
     const { data: order, error } = await supabase
       .from("orders")
-      .select("id")
+      .select("id, code")
       .eq("id", id)
       .single();
 
     if (error || !order) notFound();
     orderId = order.id;
+    code = order.code;
   }
 
   return (
-    <PageMain>
-      <OrderStoreWrapper id={orderId ?? ""}>
-        <FormGroup>
-          <FormSection>
-            <FormHeading>
-              <FormTitle>Dados do orçamento</FormTitle>
-              <FormDescription>
-                Gerencie todos os orçamentos e o fechamento dos pedidos.
-              </FormDescription>
-            </FormHeading>
-            <FormSeparator />
-            <FormContent>
-              <OrderReferenceForm />
-            </FormContent>
-          </FormSection>
-          <FormSection>
-            <FormHeading>
-              <FormTitle>Projetos inclusos</FormTitle>
-              <FormDescription>
-                Projetos que o cliente está orçando e o que está incluso.
-              </FormDescription>
-            </FormHeading>
-            <FormSeparator className="hidden lg:block" />
-            <FormContent>
-              <OrderProjects>
-                <OrderProjectsContent>
-                  <SummaryTable />
-                  <OrderProjectsActions />
-                </OrderProjectsContent>
-                <OrderProjectsTotal />
-              </OrderProjects>
-            </FormContent>
-            <FormSeparator />
-            <FormContent>
-              <OrderIncludedForm />
-            </FormContent>
-          </FormSection>
-          <FormSection>
-            <FormHeading>
-              <FormTitle>Pagamento e condições</FormTitle>
-              <FormDescription>
-                Indique como o cliente vai pagar ao finalizar o pedido.
-              </FormDescription>
-            </FormHeading>
-            <FormSeparator />
-            <FormContent>
-              <OrderPaymentForm />
-            </FormContent>
-          </FormSection>
-          <FormEmptySection />
-        </FormGroup>
-      </OrderStoreWrapper>
-    </PageMain>
+    <>
+      <OrderMenu code={code} />
+      <PageMain>
+        <OrderStoreWrapper id={orderId ?? ""}>
+          <FormGroup>
+            <FormSection>
+              <FormHeading>
+                <FormTitle>Dados do orçamento</FormTitle>
+                <FormDescription>
+                  Gerencie todos os orçamentos e o fechamento dos pedidos.
+                </FormDescription>
+              </FormHeading>
+              <FormSeparator />
+              <FormContent>
+                <OrderReferenceForm />
+              </FormContent>
+            </FormSection>
+            <FormSection>
+              <FormHeading>
+                <FormTitle>Projetos inclusos</FormTitle>
+                <FormDescription>
+                  Projetos que o cliente está orçando e o que está incluso.
+                </FormDescription>
+              </FormHeading>
+              <FormSeparator className="hidden lg:block" />
+              <FormContent>
+                <OrderProjects>
+                  <OrderProjectsContent>
+                    <SummaryTable />
+                    <OrderProjectsActions />
+                  </OrderProjectsContent>
+                  <OrderProjectsTotal />
+                </OrderProjects>
+              </FormContent>
+              <FormSeparator />
+              <FormContent>
+                <OrderIncludedForm />
+              </FormContent>
+            </FormSection>
+            <FormSection>
+              <FormHeading>
+                <FormTitle>Pagamento e condições</FormTitle>
+                <FormDescription>
+                  Indique como o cliente vai pagar ao finalizar o pedido.
+                </FormDescription>
+              </FormHeading>
+              <FormSeparator />
+              <FormContent>
+                <OrderPaymentForm />
+              </FormContent>
+            </FormSection>
+            <FormEmptySection />
+          </FormGroup>
+        </OrderStoreWrapper>
+      </PageMain>
+    </>
   );
 }
